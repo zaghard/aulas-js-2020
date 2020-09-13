d257292c72da521fd6384e98d3a99e8530a620b6
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2020-09-13 12:53:11 -0300

    duvidas em funcoes

---
+++ javascript-ES6/aula01/funcoes.js
@@ -0,0 +1,11 @@
+function getName(){
+    return "Mauricio Gonzaga Almeida";
+}
+
+function logFn(fn){
+    console.log(fn());
+}
+const logFnResult = logFn;
+
+//const arr = [logFn]
+//logFnResult(getName); 
