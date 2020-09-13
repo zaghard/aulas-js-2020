cd3e3dc8dc360bfee9c07a4c4fe936f80020a716
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2020-09-12 22:49:25 -0300

    dicionario e arrays

---
+++ Aulas/aula02/js/main.js
@@ -0,0 +1,12 @@
+
+var frutas = [{nome:'maçã', cor:'vermelha'},{nome:'uva', cor:'roxa'}]
+console.log(frutas);
+alert(frutas[1].cor);
+
+
+//var lista = ['maça', 'laranja', 'pera'];
+//lista.push('uva'); //.push para adicionar elemento a lista
+//lista.pop();
+// console.log(frutas.reverse());
+// console.log(frutas.toString());
+// console.log(frutas.join(" - "));
