c8d3a78098c345695b2ffda87679d802aaa6b7ee
zaghard <mauricioalmeida.info@gmail.com>
2021-02-06 21:27:37 -0300

    declarações de variaveis

--- Aulas/aula05/index.js
+++ Aulas/aula05/index.js
@@ -1,5 +1,11 @@
-console.log('João nasceu em 1984.');
-console.log('Em 2000 João conheceu Maria.');
-console.log('João casou-se com Maria em 2012.');
-console.log('Maria teve 1 filho com João em 2015.');
-console.log('O filho de João se chama Eduardo.');
+let nome1 = 'joão';
+let nome2 = 'Maria';
+
+
+
+
+console.log(nome1 ,' nasceu em 1984.');
+console.log('Em 2000', nome1, 'conheceu', nome2);
+console.log(nome1,'casou-se com',nome2,'em 2012.');
+console.log(nome2,'teve 1 filho com', nome1, 'em 2015.');
+console.log('O filho de', nome1, 'se chama Eduardo.');
