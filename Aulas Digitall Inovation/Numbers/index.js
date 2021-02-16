b6a287f5a5208af9b71218ed07aed1f7188fff8a
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-16 13:34:24 -0300

    instruções sobre o parseInt e ParseFloat.

---
+++ Aulas Digitall Inovation/Numbers/index.js
@@ -0,0 +1,15 @@
+const myNumber = 12.4032;
+
+//Transformar número para string
+const numberAsString = myNumber.toString();
+console.log(' Número transformado em string: ',typeof numberAsString);
+
+//Retorna número com um número de casas décimais
+const fixedTwoDecimals = myNumber.toFixed(2);
+console.log('\nNúmero com duas casas decimais:', fixedTwoDecimals);
+
+//Transforma uma string em um float.
+console.log('\nString parseada para Float:', parseFloat('13.22'));
+
+//Tranforma uma String em um Int.
+console.log('\n String parseada para Int:',parseInt('13.20'));
