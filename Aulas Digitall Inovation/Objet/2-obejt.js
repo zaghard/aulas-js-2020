3adff1fce00d9b87b658efcbaa895ca4876039ba
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-16 21:34:20 -0300

    criação de object.js

---
+++ Aulas Digitall Inovation/Objet/2-obejt.js
@@ -0,0 +1,31 @@
+const user = {
+    name: 'Mauricio',
+    lastName: 'Gonzaga Almeida'
+}
+
+//Recupera as chaves do objeto
+console.log('Propriedades do objeto user:', Object.keys(user));
+
+//Recupera os valores das chaves do objeto
+console.log('\nValores das propriedades do objeto user:', Object.values(user));
+
+//Retorna um array de arrays contendo [nome_prop, valor_prop]
+console.log('\nLista  de propriedades e valores:', Object.entries(user));
+
+//Mergear propriedades de um objeto.
+Object.assign(user, {fullName: 'Mauricio Gonzaga Almeida'});
+
+console.log('\nAdiciona a propriedade fullname no objeto user', user);
+console.log('\nRetorna um novo objeto mergeado dois ou mais objetos', Object.assign({}, user, {age: 40}));
+
+//Previne todas as alterações em um objeto.
+const newObj = { foo: 'bar' };
+Object.freeze(newObj);
+
+newObj.foo = 'changes';
+delete newObj.foo;
+newObj.bar = 'foo';
+
+console.log('\nVariável newObj após as alterações:', newObj);
+
+//Permite apenas a alteração de propriedades existentes em um objeto.
