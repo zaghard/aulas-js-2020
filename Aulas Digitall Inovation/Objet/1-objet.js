e405779f2d31092af5d68dd38017732241078a9d
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-16 21:14:09 -0300

    deletando uma propriedade.

--- Aulas Digitall Inovation/Objet/1-objet.js
+++ Aulas Digitall Inovation/Objet/1-objet.js
@@ -2,11 +2,17 @@ let user = {
     name: 'Mauricio'
 };
 //Alterando a propriedade de um objeto
+//console.log(user);
+
 user.name = 'Outro nome 1';
+//console.log(user);
+
 user['name'] = 'Outro nome 2';
+//console.log(user);
 
 const prop = 'name';
 user[prop] = 'Outro nome 3';
+//console.log(user);
 
 function getProp(prop){
     return user[prop]
@@ -14,6 +20,7 @@ function getProp(prop){
 
 //Criando uma propriedade
 user.lastName = 'Gonzaga Almeida';
-
+console.log(user);
 //Deletando uma propriedade
 delete user.name;
+console.log(user);
