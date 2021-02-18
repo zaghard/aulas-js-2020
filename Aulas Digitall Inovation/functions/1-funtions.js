f085e38800e50ccac38d7ab595c3cc1f2e2453ac
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-18 10:57:42 -0300

    declaração de funções para funções.

--- Aulas Digitall Inovation/functions/1-funtions.js
+++ Aulas Digitall Inovation/functions/1-funtions.js
@@ -15,8 +15,29 @@ fn.prop = 'Posso criar propriedades';
 console.log(fn());
 console.log(fn.prop);
 
-//Recebr parámetros
+//Receber parámetros
 const logValues = value => console.log(value);
 const logFnResult = fnParam => console.log(fnParam());
 
 logFnResult(fn);
+
+//Receber e retorna funções no ES6
+/*const controlFnExec => fnParam => allowed => {
+    if(allowed) {
+        fnParam( );
+    }
+}
+*/
+const handleFnExcution = controlFnExec(fn);
+
+handleFnExcution(true); //Executará a função fn.
+handleFnExcution( ); //Não executará a função fn.
+
+// controlFnExec como função
+function controlFnExec(fnParam) {
+    return function(allowed) {
+        if (allowed) {
+            fnParam( );
+        }
+    }
+}
