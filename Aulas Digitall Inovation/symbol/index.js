022e1278e9c471c64879310f4a158290aa38bf94
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-17 21:09:16 -0300

    criação de array

--- Aulas Digitall Inovation/symbol/index.js
+++ Aulas Digitall Inovation/symbol/index.js
@@ -3,3 +3,15 @@ const symbol2 = Symbol();
 
 //Symbols são únicos
 console.log('symbol1 é igual a symbol2:',symbol1 === symbol2);
+
+//Previnir conflito entre nomes de prooriedades
+const nameSymbol1 = Symbol('name');
+const nameSymbol2 = Symbol('name');
+
+const user = {
+    [nameSymbol1] : 'Mauricio',
+    [nameSymbol2] : 'Gonzaga',
+    lastName: 'Gonzaga Almeida'
+
+}
+console.log(user);
