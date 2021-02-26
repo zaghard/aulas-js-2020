c4028c555b0a2ca0eedac667dfd713699a865600
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-25 21:17:17 -0300

    forEach, filter, map, isArray

--- Aulas Digitall Inovation/aulasJS/arrays/index.js
+++ Aulas Digitall Inovation/aulasJS/arrays/index.js
@@ -22,7 +22,25 @@ const persons = [
         gender:gender.WOMAN
     }
         
-    
-];
+ ];
+
 //retornar a quantidade de itens de um array
 console.log('Itens:', persons.length);
+
+//Verifica se é array
+console.log('A variável persons é um array:', Array.isArray(persons));
+
+//Iterar os itens do array
+persons.forEach((person, index, arr) => {
+    console.log(`Nome: ${person.name} index: ${index}`, arr);
+});
+//Filtrar array
+const mens = persons.filter(person => person.gender === gender.MAN);
+console.log('\nNova lista apenas com homens:', mens);
+
+//Retorna um novo
+const personsWithCourse = persons.map( person =>{
+    person.course = 'Introdução ao Javascript';
+    return person;
+
+})
