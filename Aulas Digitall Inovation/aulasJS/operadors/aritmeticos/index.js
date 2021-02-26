a56c5ce6c7d892219f5c93f1bc851665ea8a6721
Mauricio G. Almeida <mauricioalmeida.info@gmail.com>
2021-02-25 22:32:58 -0300

    operadores aritmeticos

---
+++ Aulas Digitall Inovation/aulasJS/operadors/aritmeticos/index.js
@@ -0,0 +1,24 @@
+//Modulo (%)
+//Operador binário, retorna o inteiro restante da divisão
+12 % 5 // retorna 2
+
+//incremento (++) e decremento ( -- )
+x++
+++x
+
+--x
+x--
+
+//Negação (-) e  Adição ( + )
+-3
++'3' //retorna 3
++true //retorna 1
++false // retorna 0
+-true // retorna -1
+
+//Operador de exponeciação (**)
+2 ** 3 // retorna 8
+10 ** -1 // retorna 0.1
+
+//Operador de agrupamento ()
+2 * (3 + 2)
